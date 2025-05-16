c849949c3ae5668883b87547d2b54342b99e3e8b
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2025-05-15 21:03:43 -0300

    feat: set FirstScreen as the home widget and remove placeholder text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:imc/secondScreen.dart';
+import 'package:imc/firstScreen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +12,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: FirstScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
